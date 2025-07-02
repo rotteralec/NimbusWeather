30e72fd0fd505b51c235c9107a575b0a6ccfda13
rotteralec <rotteralec@gmail.com>
2025-07-02 15:56:39 -0400

    Initial Commit

---
+++ NimbusWeatherTests/NimbusWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusWeatherTests.swift
+//  NimbusWeatherTests
+//
+//  Created by Al Rotter on 7/2/25.
+//
+
+import Testing
+@testable import NimbusWeather
+
+struct NimbusWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
